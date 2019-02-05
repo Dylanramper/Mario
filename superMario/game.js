7c2e75dec649089e6d80176b1f0052ae350066e0
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2019-02-05 16:35:13 -0500

    game

--- superMario/game.js
+++ superMario/game.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 	var canvas = $("canvas");
 	var context = canvas.get(0).getContext("2d");
-	
 
 	var JUMP = 40;
 	var RIGHT = 39;
@@ -11,11 +10,16 @@ $(document).ready(function () {
 
 	var rightInnerBoundary = (500 / 2) + (500 / 2);
 
+	var mario = new Mario();
+	mario.image.src = mario.imageSource;
+
 	var background = new Background(context, canvas.width(), canvas.height());
 	background.image.src = background.imageSource;
 
 	var backgroundCollisions = [];
 	backgroundCollisions.push(new collisionBox(background.x, 520, 2210, 80, context));
+
+	
 
 	window.addEventListener("keydown", keydownHandler, false);
 	window.addEventListener("keyup", keyupHandler, false);
@@ -25,11 +29,16 @@ $(document).ready(function () {
 
 			case RIGHT:
 				background.vx = -5;
-				//console.log(background.x);
+				mario.vx = 5;
 				if (background.x < -6050) {
 					background.vx = 0;
 
 				}
+				break;
+
+			case LEFT:
+				mario.vx = -5;
+				background.vx = 0;
 				break;
 
 		}
@@ -39,22 +48,80 @@ $(document).ready(function () {
 	function keyupHandler(event) {
 		if (event.keyCode === LEFT || event.keyCode === RIGHT) {
 			background.vx = 0;
+			mario.vx = 0;
 		}
 
 	}
-	
+
+	function PlayGame() {
+
+		if (moveLeft && !moveRight) {
+			mario.accelerationX = -0.2;
+			mario.friction = 1;
+		}
+
+
+		if (moveRight && !moveLeft) {
+			mario.accelerationX = 0.2;
+			mario.friction = 1;
+		}
+
+		if (jump && mario.isOnGround) {
+			mario.vy += mario.jumpForce;
+			mario.isOnGround = false;
+			mario.friction = 1;
+		}
+
+
+		if (!moveLeft && !moveRight) {
+			mario.accelerationX = 0;
+			mario.friction = 0.96;
+			mario.gravity = 0.3;
+		}
+
+
+		mario.vx += mario.accelerationX;
+		mario.vy += mario.accelerationY;
+
+		//Apply the friction
+		if (mario.isOnGround) {
+			mario.vx *= mario.friction;
+		}
+
+		//Apply the gravity
+		mario.vy += mario.gravity;
+
+		//Limit the speed
+
+		if (mario.vx > mario.speedLimit) {
+			mario.vx = mario.speedLimit;
+		}
+
+		if (mario.vx < -mario.speedLimit) {
+			mario.vx = -mario.speedLimit;
+		}
+
+		if (mario.vy > mario.speedLimit * 2) {
+			mario.vy = mario.speedLimit * 2;
+		}
+
+		//Move mario
+		mario.x += mario.vx;
+		mario.y += mario.vy;
+	}
 
 	//var mario = new Mario();
 	//mario.Image.src = mario.imageSource;
 
 	function Update() {
+		console.log(backgroundCollisions[0]);
 		requestAnimationFrame(Update, canvas);
 
 		//mario.Update();
 		background.Update();
 
 		for (var i = 0; i < backgroundCollisions.length; i++) {
-			//backgroundCollisions[i].Update(background.x, background.y);
+			backgroundCollisions[i].Update(background.x);
 		}
 		//Check wether the cat moved to the edges of the inner boundaries.
 		//if (mario.x < rightInnerBoundary) {
@@ -71,6 +138,7 @@ $(document).ready(function () {
 	}
 
 	function Render() {
+		mario.Render();
 		context.clearRect(0,0,canvas.width, canvas.height);
 		background.Render();
 		for (var i = 0; i < backgroundCollisions.length; i++) {
--- superMario/mario.js
+++ superMario/mario.js
@@ -1,24 +1,47 @@
-var mario = (function () {
-	this.sourceX = 0;
-	this.sourceY = 0;
-	this.sourceWidth = 447;
-	this.sourceHeight = 687;
+ var Player = (function (x, y, context) {
+    this.sourceX = 0;
+    this.sourceY = 64;
+    this.sourceWidth = 32;
+    this.sourceHeight = 32;
+    this.x = 200;
+    this.y = 488;
+    this.width = 32;
+    this.height = 32;
+    this.vx = 0;
+    this.vy = 0;
+    this.visible = true;
+    this.accelerationX = 0;
+    this.accelerationY = 0;
+    this.speedLimit = 5;
+    this.friction = 0.96;
+    this.gravity = 0.4;
+    this.isOnGround = undefined;
+    this.jumpForce = -10;
+    this.image = new Image();
+    this.image.src = ("Images/mario.png");
 
-	this.x = 81;
-	this.y = 35;
-	this.width = 16;
-	this.height = 16;
 
-	this.Image = new Image();
-	this.imageSource = "Images/marioStates.png";
+    this.CenterX = function () {
+        return this.x + (this.width / 2);
+    }
+    this.CenterY = function () {
+        return this.y + (this.height / 2);
+    }
+    this.halfWidth = function () {
+        return this.width / 2;
+    }
+    this.halfHeight = function () {
+        return this.height / 2;
+    }
 
-	this.vx = 0;
-	this.vy = 0;
-	this.width = 16;
-	this.height = 16;
+    this.Update = function () {
 
-	this.Update = function () {
-		this.x += this.vx;
-		this.y += this.vy;
-	}
+    }
+
+    this.Render = function () {
+        context.drawImage(this.image, this.sourceX, this.sourceY, this.sourceWidth, this.sourceHeight, this.x, this.y, this.width, this.height);
+    }
 });
+
+
+
