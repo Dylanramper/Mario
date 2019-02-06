9a332daeb1ee8b58a2de5507d356867e56c791d6
Epikkat <Epikkat@users.noreply.github.com>
2019-02-06 15:29:27 -0500

    meow

--- superMario/game.js
+++ superMario/game.js
@@ -18,10 +18,13 @@ $(document).ready(function () {
 	var backgroundCollisions = [];
 	backgroundCollisions.push(new collisionBox(background.x, 520, 2210, 80, context));
 
-	var block = new Block(context, 515, 370);
-	//var block = [];
-	//block.push(new Block(block.x, 
-	
+	//var block = new Block(context, 515, 370);
+	var block = [];
+	block.push(new Block(context, 515, 370));
+	block.push(new Block(context, 651, 370));
+	block.push(new Block(context, 715, 370));
+	block.push(new Block(context, 779, 370));
+	block.push(new Block(context, 715, 262));
 
 	window.addEventListener("keydown", keydownHandler, false);
 	window.addEventListener("keyup", keyupHandler, false);
@@ -57,12 +60,13 @@ $(document).ready(function () {
 
 	function Update() {
 		requestAnimationFrame(Update, canvas);
-
 		//mario.Update();
 		background.Update();
-
 		for (var i = 0; i < backgroundCollisions.length; i++) {
 			//backgroundCollisions[i].Update(background.x, background.y);
+		}
+		for (var i = 0; i < block.length; i++) {
+			block[i].Update(background.x, background.y);
 		}
 		//Check wether the cat moved to the edges of the inner boundaries.
 		//if (mario.x < rightInnerBoundary) {
@@ -85,9 +89,9 @@ $(document).ready(function () {
 			backgroundCollisions[i].Render();
 		}
 	
-		block.Render();
-		//for (var i = 0; i < blockCollisions.length; i++) {
-			//blockCollisions[i].Render();
+		for (var i = 0; i < block.length; i++) {
+			block[i].Render();
+		}
 	}
 
 	Update();
